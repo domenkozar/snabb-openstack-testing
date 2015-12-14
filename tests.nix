98035b59585665f8e405f3778bb99c9df5235f1a
Domen Kožar <domen@dev.si>
2015-12-14 14:00:54 +0100

    comment out tests for now

--- tests.nix
+++ tests.nix
@@ -36,74 +36,74 @@ makeTest {
 
     subtest "VM with NIC", sub {
       $allinone->execute('/root/tests/zone_test_01.sh');
-    }
+    };
 
-    subtest "VM with 2xNIC (low bandwidth)", sub {
-      $allinone->execute('/root/tests/zone_test_02.sh');
-    }
+    #subtest "VM with 2xNIC (low bandwidth)", sub {
+    #  $allinone->execute('/root/tests/zone_test_02.sh');
+    #};
 
-    subtest "VM with 2xNIC (high bandwidth)", sub {
-      $allinone->execute('/root/tests/zone_test_03.sh');
-    }
+    #subtest "VM with 2xNIC (high bandwidth)", sub {
+    #  $allinone->execute('/root/tests/zone_test_03.sh');
+    #};
 
-    subtest "2xVM on same physical port", sub {
-      $allinone->execute('/root/tests/zone_test_04.sh');
-    }
+    #subtest "2xVM on same physical port", sub {
+    #  $allinone->execute('/root/tests/zone_test_04.sh');
+    #};
 
-    subtest "2xVM on different physical port", sub {
-      $allinone->execute('/root/tests/zone_test_05.sh');
-    }
+    #subtest "2xVM on different physical port", sub {
+    #  $allinone->execute('/root/tests/zone_test_05.sh');
+    #};
 
-    subtest "2xVM with security group restrictions", sub {
-      $allinone->execute('/root/tests/zone_test_06.sh');
-    }
+    #subtest "2xVM with security group restrictions", sub {
+    #  $allinone->execute('/root/tests/zone_test_06.sh');
+    #};
 
-    subtest "2xVM bandwidth restriction", sub {
-      $allinone->execute('/root/tests/zone_test_07.sh');
-    }
+    #subtest "2xVM bandwidth restriction", sub {
+    #  $allinone->execute('/root/tests/zone_test_07.sh');
+    #};
 
-    subtest VM with L2TPv3", sub {
-      $allinone->execute('/root/tests/zone_test_08.sh');
-    }
+    #subtest VM with L2TPv3", sub {
+    #  $allinone->execute('/root/tests/zone_test_08.sh');
+    #};
 
-    subtest "multiple VMs", sub {
-      $allinone->execute('/root/tests/zone_test_09.sh');
-    }
+    #subtest "multiple VMs", sub {
+    #  $allinone->execute('/root/tests/zone_test_09.sh');
+    #};
 
-    subtest "VM with NIC, invalid zone", sub {
-      $allinone->execute('/root/tests/zone_test_10.sh');
-    }
+    #subtest "VM with NIC, invalid zone", sub {
+    #  $allinone->execute('/root/tests/zone_test_10.sh');
+    #};
 
-    subtest "2xVM on different physical port, restart neutron", sub {
-      $allinone->execute('/root/tests/zone_test_11.sh');
-    }
+    #subtest "2xVM on different physical port, restart neutron", sub {
+    #  $allinone->execute('/root/tests/zone_test_11.sh');
+    #};
 
-    subtest "2xVM on different physical port, restart nova", sub {
-      $allinone->execute('/root/tests/zone_test_12.sh');
-    }
+    #subtest "2xVM on different physical port, restart nova", sub {
+    #  $allinone->execute('/root/tests/zone_test_12.sh');
+    #};
 
-    subtest "2xVM on different physical port, restart snabb services", sub {
-      $allinone->execute('/root/tests/zone_test_13.sh');
-    }
+    #subtest "2xVM on different physical port, restart snabb services", sub {
+    #  $allinone->execute('/root/tests/zone_test_13.sh');
+    #};
 
-    subtest "L2TPv3 between 2 VMs", sub {
-      $allinone->execute('/root/tests/zone_test_14.sh');
-    }
+    #subtest "L2TPv3 between 2 VMs", sub {
+    #  $allinone->execute('/root/tests/zone_test_14.sh');
+    #};
 
-    subtest "L2TPv3 between 2 VMs add remove tunnel", sub {
-      $allinone->execute('/root/tests/zone_test_15.sh');
-    }
+    #subtest "L2TPv3 between 2 VMs add remove tunnel", sub {
+    #  $allinone->execute('/root/tests/zone_test_15.sh');
+    #};
 
-    subtest "2xVM with security group restrictions", sub {
-      $allinone->execute('/root/tests/zone_test_16.sh');
-    }
+    #subtest "2xVM with security group restrictions", sub {
+    #  $allinone->execute('/root/tests/zone_test_16.sh');
+    #};
 
-    subtest "2xVM on same physical port IPv4", sub {
-      $allinone->execute('/root/tests/zone_test_17.sh');
-    }
+    #subtest "2xVM on same physical port IPv4", sub {
+    #  $allinone->execute('/root/tests/zone_test_17.sh');
+    #};
 
-    subtest "2xVM with security group restrictions and stateless filtering", sub {
-      $allinone->execute('/root/tests/zone_test_18.sh');
-    }
+    #subtest "2xVM with security group restrictions and stateless filtering", sub {
+    #  $allinone->execute('/root/tests/zone_test_18.sh');
+    #};
   '';
 }
