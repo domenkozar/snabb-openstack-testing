efae2cd72958d1df39e311f2e83b4e64f2339aca
Domen Kožar <domen@dev.si>
2016-02-11 20:31:59 +0100

    openstack-libvirt.nix: bump hdd, memory and add pci devices

--- openstack-libvirt.nix
+++ openstack-libvirt.nix
@@ -2,9 +2,27 @@
   allinone = { config, pkgs, ... }:
     {
       deployment.targetEnv = "libvirtd";
-      deployment.libvirtd.memorySize = 2500;
-      deployment.libvirtd.baseImageSize = 3;
-      deployment.libvirtd.extraDevicesXML = "";
+      deployment.libvirtd.memorySize = 16 * 1024;
+      deployment.libvirtd.baseImageSize = 30;
+      # PCI mapping https://docs.fedoraproject.org/en-US/Fedora/13/html/Virtualization_Guide/chap-Virtualization-PCI_passthrough.html
+      deployment.libvirtd.extraDevicesXML = ''
+        <hostdev mode='subsystem' type='pci' managed='yes'>
+          <driver name='kvm' />
+          <rom bar='off'/>
+          <source>
+            <address type="pci" domain='0x0000' bus='0x3' slot='0x00' function='0x0'/>
+          </source>
+          <address type="pci" domain='0x0000' bus='0x0' slot='0x15' function='0x0'/>
+        </hostdev>
+        <hostdev mode='subsystem' type='pci' managed='yes'>
+          <driver name='kvm' />
+          <rom bar='off'/>
+          <source>
+            <address type="pci" domain='0x0000' bus='0x3' slot='0x00' function='0x1'/>
+          </source>
+          <address type="pci" domain='0x0000' bus='0x0' slot='0x16' function='0x0'/>
+        </hostdev>
+      '';
       deployment.libvirtd.headless = true;
     };
 }
