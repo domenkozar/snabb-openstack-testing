aa1f8c130e3d975c93130af538948c2064f14dac
Domen Kožar <domen@dev.si>
2016-02-11 20:30:58 +0100

    docker-entrypoint.sh: add comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-nix-build tests.nix -I /snabb/ -A driver
+
+# we rebuild the tests with environment variables about pci devices
+nix-build tests.nix -I /snabb/ -A driver --option build-use-substitutes false
+
+# we run tests as separate step as qemu needs to be run to do pci assignment
 tests='eval $ENV{testScript}; die $@ if $@;' ./result/bin/nixos-test-driver
 
